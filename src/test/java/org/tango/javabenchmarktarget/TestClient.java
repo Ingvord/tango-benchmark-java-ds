6a6c1fcc67ecd1644173482856b8cb288287cd38
ingvord <mail@ingvord.ru>
2019-12-11 16:20:16 +0100

    Progress

--- src/test/java/org/tango/javabenchmarktarget/TestClient.java
+++ src/test/java/org/tango/javabenchmarktarget/TestClient.java
@@ -17,35 +17,40 @@ import java.util.concurrent.atomic.AtomicLong;
  * @since 11.12.2019
  */
 public class TestClient {
+
+    public static final int NUMBER_OF_CLIENTS = 128;
+
     @Test
     @Ignore
     public void test() throws Exception {
-        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_CLIENTS);
 
         final AtomicLong counter = new AtomicLong(0L);
         final AtomicLong errors = new AtomicLong(0L);
 
         final AtomicBoolean finish = new AtomicBoolean(false);
 
-        executorService.submit(()->{
-            TangoAttribute attribute = null;
-            try {
-                attribute = new TangoAttribute("tango://hzgxenvtest:10000/development/benchmark/0/BenchmarkScalarAttribute");
-            } catch (DevFailed devFailed) {
-                errors.incrementAndGet();
-                return;
-            }
-
-
-            while (!finish.get()) {
+        for(int i = 0; i< NUMBER_OF_CLIENTS; ++i) {
+            executorService.submit(() -> {
+                TangoAttribute attribute = null;
                 try {
-                    attribute.write(3.14);
-                    counter.incrementAndGet();
-                } catch (DevFailed e) {
+                    attribute = new TangoAttribute("tango://hzgxenvtest:10000/development/benchmark/0/BenchmarkScalarAttribute");
+                } catch (DevFailed devFailed) {
                     errors.incrementAndGet();
+                    return;
                 }
-            }
-        });
+
+
+                while (!finish.get()) {
+                    try {
+                        attribute.write(3.14);
+                        counter.incrementAndGet();
+                    } catch (DevFailed e) {
+                        errors.incrementAndGet();
+                    }
+                }
+            });
+        }
 
 
         Thread.sleep(3_600_000);
